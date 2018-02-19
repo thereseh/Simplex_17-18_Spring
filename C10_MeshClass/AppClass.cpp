4447daae4948db452b08d018022e090c0ce4409d
thereseh <tah5279@g.rit.edu>
2018-02-18 23:05:57 -0500

    E05 Deliverable

--- C10_MeshClass/AppClass.cpp
+++ C10_MeshClass/AppClass.cpp
@@ -1,13 +1,22 @@
 #include "AppClass.h"
 void Application::InitVariables(void)
-{
+{	// 46
 	//Make MyMesh object
-	m_pMesh = new MyMesh();
+
+	for (int i = 0; i < 46; i++) {
+		MyMesh* m_pMesh = new MyMesh();
+		m_pMesh->GenerateCube(2.0f, C_BLACK);
+
+		m_mesh.push_back(m_pMesh);
+	}
+	
+	/*m_pMesh = new MyMesh();
 	m_pMesh->GenerateCube(2.0f, C_BROWN);
 
 	//Make MyMesh object
 	m_pMesh1 = new MyMesh();
 	m_pMesh1->GenerateCube(1.0f, C_WHITE);
+	*/
 }
 void Application::Update(void)
 {
@@ -25,9 +34,63 @@ void Application::Display(void)
 	// Clear the screen
 	ClearScreen();
 
-	m_pMesh->Render(m_pCameraMngr->GetProjectionMatrix(), m_pCameraMngr->GetViewMatrix(), ToMatrix4(m_qArcBall));
-	m_pMesh1->Render(m_pCameraMngr->GetProjectionMatrix(), m_pCameraMngr->GetViewMatrix(), glm::translate(vector3( 3.0f, 0.0f, 0.0f)));
-		
+	static float fPos = 0.0f;
+
+	m_m4Cube = glm::translate(vector3(fPos, 0.0f, 0.0f));
+
+	for (int i = 0; i < 46; i++) {
+
+		if (i == 0) m_mesh[i]->Render(m_pCameraMngr->GetProjectionMatrix(), m_pCameraMngr->GetViewMatrix(), glm::translate(vector3(-8.0f, 8.0f, -20.0f)) * m_m4Cube);
+		if (i == 1) m_mesh[i]->Render(m_pCameraMngr->GetProjectionMatrix(), m_pCameraMngr->GetViewMatrix(), glm::translate(vector3(4.0f, 8.0f, -20.0f)) * m_m4Cube);
+		if (i == 2) m_mesh[i]->Render(m_pCameraMngr->GetProjectionMatrix(), m_pCameraMngr->GetViewMatrix(), glm::translate(vector3(-6.0f, 6.0f, -20.0f)) * m_m4Cube);
+		if (i == 3) m_mesh[i]->Render(m_pCameraMngr->GetProjectionMatrix(), m_pCameraMngr->GetViewMatrix(), glm::translate(vector3(2.0f, 6.0f, -20.0f)) * m_m4Cube);
+		if (i == 4) m_mesh[i]->Render(m_pCameraMngr->GetProjectionMatrix(), m_pCameraMngr->GetViewMatrix(), glm::translate(vector3(-8.0f, 4.0f, -20.0f)) * m_m4Cube);
+		if (i == 5) m_mesh[i]->Render(m_pCameraMngr->GetProjectionMatrix(), m_pCameraMngr->GetViewMatrix(), glm::translate(vector3(-6.0f, 4.0f, -20.0f)) * m_m4Cube);
+		if (i == 6) m_mesh[i]->Render(m_pCameraMngr->GetProjectionMatrix(), m_pCameraMngr->GetViewMatrix(), glm::translate(vector3(-4.0f, 4.0f, -20.0f)) * m_m4Cube);
+		if (i == 7) m_mesh[i]->Render(m_pCameraMngr->GetProjectionMatrix(), m_pCameraMngr->GetViewMatrix(), glm::translate(vector3(-2.0f, 4.0f, -20.0f)) * m_m4Cube);
+		if (i == 8) m_mesh[i]->Render(m_pCameraMngr->GetProjectionMatrix(), m_pCameraMngr->GetViewMatrix(), glm::translate(vector3(0.0f, 4.0f, -20.0f)) * m_m4Cube);
+		if (i == 9) m_mesh[i]->Render(m_pCameraMngr->GetProjectionMatrix(), m_pCameraMngr->GetViewMatrix(), glm::translate(vector3(2.0f, 4.0f, -20.0f)) * m_m4Cube);
+		if (i == 10) m_mesh[i]->Render(m_pCameraMngr->GetProjectionMatrix(), m_pCameraMngr->GetViewMatrix(), glm::translate(vector3(4.0f, 4.0f, -20.0f)) * m_m4Cube);
+		if (i == 11) m_mesh[i]->Render(m_pCameraMngr->GetProjectionMatrix(), m_pCameraMngr->GetViewMatrix(), glm::translate(vector3(-8.0f, 2.0f, -20.0f)) * m_m4Cube);
+		if (i == 12) m_mesh[i]->Render(m_pCameraMngr->GetProjectionMatrix(), m_pCameraMngr->GetViewMatrix(), glm::translate(vector3(-10.0f, 2.0f, -20.0f)) * m_m4Cube);
+		if (i == 13) m_mesh[i]->Render(m_pCameraMngr->GetProjectionMatrix(), m_pCameraMngr->GetViewMatrix(), glm::translate(vector3(-4.0f, 2.0f, -20.0f)) * m_m4Cube);
+		if (i == 14) m_mesh[i]->Render(m_pCameraMngr->GetProjectionMatrix(), m_pCameraMngr->GetViewMatrix(), glm::translate(vector3(-2.0f, 2.0f, -20.0f)) * m_m4Cube);
+		if (i == 15) m_mesh[i]->Render(m_pCameraMngr->GetProjectionMatrix(), m_pCameraMngr->GetViewMatrix(), glm::translate(vector3(0.0f, 2.0f, -20.0f)) * m_m4Cube);
+		if (i == 16) m_mesh[i]->Render(m_pCameraMngr->GetProjectionMatrix(), m_pCameraMngr->GetViewMatrix(), glm::translate(vector3(4.0f, 2.0f, -20.0f)) * m_m4Cube);
+		if (i == 17) m_mesh[i]->Render(m_pCameraMngr->GetProjectionMatrix(), m_pCameraMngr->GetViewMatrix(), glm::translate(vector3(6.0f, 2.0f, -20.0f)) * m_m4Cube);
+		if (i == 18) m_mesh[i]->Render(m_pCameraMngr->GetProjectionMatrix(), m_pCameraMngr->GetViewMatrix(), glm::translate(vector3(-12.0f, 0.0f, -20.0f)) * m_m4Cube);
+		if (i == 19) m_mesh[i]->Render(m_pCameraMngr->GetProjectionMatrix(), m_pCameraMngr->GetViewMatrix(), glm::translate(vector3(-10.0f, 0.0f, -20.0f)) * m_m4Cube);
+		if (i == 20) m_mesh[i]->Render(m_pCameraMngr->GetProjectionMatrix(), m_pCameraMngr->GetViewMatrix(), glm::translate(vector3(-8.0f, 0.0f, -20.0f)) * m_m4Cube);
+		if (i == 21) m_mesh[i]->Render(m_pCameraMngr->GetProjectionMatrix(), m_pCameraMngr->GetViewMatrix(), glm::translate(vector3(-6.0f, 0.0f, -20.0f)) * m_m4Cube);
+		if (i == 22) m_mesh[i]->Render(m_pCameraMngr->GetProjectionMatrix(), m_pCameraMngr->GetViewMatrix(), glm::translate(vector3(-4.0f, 0.0f, -20.0f)) * m_m4Cube);
+		if (i == 23) m_mesh[i]->Render(m_pCameraMngr->GetProjectionMatrix(), m_pCameraMngr->GetViewMatrix(), glm::translate(vector3(-2.0f, 0.0f, -20.0f)) * m_m4Cube);
+		if (i == 24) m_mesh[i]->Render(m_pCameraMngr->GetProjectionMatrix(), m_pCameraMngr->GetViewMatrix(), glm::translate(vector3(0.0f, 0.0f, -20.0f)) * m_m4Cube);
+		if (i == 25) m_mesh[i]->Render(m_pCameraMngr->GetProjectionMatrix(), m_pCameraMngr->GetViewMatrix(), glm::translate(vector3(2.0f, 0.0f, -20.0f)) * m_m4Cube);
+		if (i == 26) m_mesh[i]->Render(m_pCameraMngr->GetProjectionMatrix(), m_pCameraMngr->GetViewMatrix(), glm::translate(vector3(4.0f, 0.0f, -20.0f)) * m_m4Cube);
+		if (i == 27) m_mesh[i]->Render(m_pCameraMngr->GetProjectionMatrix(), m_pCameraMngr->GetViewMatrix(), glm::translate(vector3(6.0f, 0.0f, -20.0f)) * m_m4Cube);
+		if (i == 28) m_mesh[i]->Render(m_pCameraMngr->GetProjectionMatrix(), m_pCameraMngr->GetViewMatrix(), glm::translate(vector3(8.0f, 0.0f, -20.0f)) * m_m4Cube);
+		if (i == 29) m_mesh[i]->Render(m_pCameraMngr->GetProjectionMatrix(), m_pCameraMngr->GetViewMatrix(), glm::translate(vector3(-12.0f, -2.0f, -20.0f)) * m_m4Cube);
+		if (i == 30) m_mesh[i]->Render(m_pCameraMngr->GetProjectionMatrix(), m_pCameraMngr->GetViewMatrix(), glm::translate(vector3(-8.0f, -2.0f, -20.0f)) * m_m4Cube);
+		if (i == 31) m_mesh[i]->Render(m_pCameraMngr->GetProjectionMatrix(), m_pCameraMngr->GetViewMatrix(), glm::translate(vector3(-6.0f, -2.0f, -20.0f)) * m_m4Cube);
+		if (i == 32) m_mesh[i]->Render(m_pCameraMngr->GetProjectionMatrix(), m_pCameraMngr->GetViewMatrix(), glm::translate(vector3(-4.0f, -2.0f, -20.0f)) * m_m4Cube);
+		if (i == 33) m_mesh[i]->Render(m_pCameraMngr->GetProjectionMatrix(), m_pCameraMngr->GetViewMatrix(), glm::translate(vector3(-2.0f, -2.0f, -20.0f)) * m_m4Cube);
+		if (i == 34) m_mesh[i]->Render(m_pCameraMngr->GetProjectionMatrix(), m_pCameraMngr->GetViewMatrix(), glm::translate(vector3(0.0f, -2.0f, -20.0f)) * m_m4Cube);
+		if (i == 35) m_mesh[i]->Render(m_pCameraMngr->GetProjectionMatrix(), m_pCameraMngr->GetViewMatrix(), glm::translate(vector3(2.0f, -2.0f, -20.0f)) * m_m4Cube);
+		if (i == 36) m_mesh[i]->Render(m_pCameraMngr->GetProjectionMatrix(), m_pCameraMngr->GetViewMatrix(), glm::translate(vector3(4.0f, -2.0f, -20.0f)) * m_m4Cube);
+		if (i == 37) m_mesh[i]->Render(m_pCameraMngr->GetProjectionMatrix(), m_pCameraMngr->GetViewMatrix(), glm::translate(vector3(8.0f, -2.0f, -20.0f)) * m_m4Cube);
+		if (i == 38) m_mesh[i]->Render(m_pCameraMngr->GetProjectionMatrix(), m_pCameraMngr->GetViewMatrix(), glm::translate(vector3(8.0f, -4.0f, -20.0f)) * m_m4Cube);
+		if (i == 39) m_mesh[i]->Render(m_pCameraMngr->GetProjectionMatrix(), m_pCameraMngr->GetViewMatrix(), glm::translate(vector3(-12.0f, -4.0f, -20.0f)) * m_m4Cube);
+		if (i == 40) m_mesh[i]->Render(m_pCameraMngr->GetProjectionMatrix(), m_pCameraMngr->GetViewMatrix(), glm::translate(vector3(-8.0f, -4.0f, -20.0f)) * m_m4Cube);
+		if (i == 41) m_mesh[i]->Render(m_pCameraMngr->GetProjectionMatrix(), m_pCameraMngr->GetViewMatrix(), glm::translate(vector3(4.0f, -4.0f, -20.0f)) * m_m4Cube);
+		if (i == 42) m_mesh[i]->Render(m_pCameraMngr->GetProjectionMatrix(), m_pCameraMngr->GetViewMatrix(), glm::translate(vector3(-6.0f, -6.0f, -20.0f)) * m_m4Cube);
+		if (i == 43) m_mesh[i]->Render(m_pCameraMngr->GetProjectionMatrix(), m_pCameraMngr->GetViewMatrix(), glm::translate(vector3(-4.0f, -6.0f, -20.0f)) * m_m4Cube);
+		if (i == 44) m_mesh[i]->Render(m_pCameraMngr->GetProjectionMatrix(), m_pCameraMngr->GetViewMatrix(), glm::translate(vector3(0.0f, -6.0f, -20.0f)) * m_m4Cube);
+		if (i == 45) m_mesh[i]->Render(m_pCameraMngr->GetProjectionMatrix(), m_pCameraMngr->GetViewMatrix(), glm::translate(vector3(2.0f, -6.0f, -20.0f)) * m_m4Cube);
+
+	}
+
+	fPos += 0.1f;
+
 	// draw a skybox
 	m_pMeshMngr->AddSkyboxToRenderList();
 	
--- C10_MeshClass/AppClass.h
+++ C10_MeshClass/AppClass.h
@@ -14,8 +14,82 @@ Date: 2017/05
 
 class Application
 {
+	matrix4 m_m4Cube = IDENTITY_M4;
+
+	std::vector<MyMesh*> m_mesh;
+
+
 	MyMesh* m_pMesh = nullptr;
 	MyMesh* m_pMesh1 = nullptr;
+
+	MyMesh* m_pMesh2 = nullptr;
+	MyMesh* m_pMesh3 = nullptr;
+
+	MyMesh* m_pMesh4 = nullptr;
+	MyMesh* m_pMesh5 = nullptr;
+
+	MyMesh* m_pMesh6 = nullptr;
+	MyMesh* m_pMesh7 = nullptr;
+
+	MyMesh* m_pMesh8 = nullptr;
+	MyMesh* m_pMesh9 = nullptr;
+
+	MyMesh* m_pMesh10 = nullptr;
+	MyMesh* m_pMesh11 = nullptr;
+
+	MyMesh* m_pMesh12 = nullptr;
+	MyMesh* m_pMesh13 = nullptr;
+
+	MyMesh* m_pMesh14 = nullptr;
+	MyMesh* m_pMesh15 = nullptr;
+
+	MyMesh* m_pMesh16 = nullptr;
+	MyMesh* m_pMesh17 = nullptr;
+
+	MyMesh* m_pMesh18 = nullptr;
+	MyMesh* m_pMesh19 = nullptr;
+
+	MyMesh* m_pMesh20 = nullptr;
+	MyMesh* m_pMesh21 = nullptr;
+
+	MyMesh* m_pMesh22 = nullptr;
+	MyMesh* m_pMesh23 = nullptr;
+
+	MyMesh* m_pMesh24 = nullptr;
+	MyMesh* m_pMesh25 = nullptr;
+
+	MyMesh* m_pMesh26 = nullptr;
+	MyMesh* m_pMesh27 = nullptr;
+
+	MyMesh* m_pMesh28 = nullptr;
+	MyMesh* m_pMesh29 = nullptr;
+
+	MyMesh* m_pMesh30 = nullptr;
+	MyMesh* m_pMesh31 = nullptr;
+
+	MyMesh* m_pMesh32 = nullptr;
+	MyMesh* m_pMesh33 = nullptr;
+
+	MyMesh* m_pMesh34 = nullptr;
+	MyMesh* m_pMesh35 = nullptr;
+
+	MyMesh* m_pMesh36 = nullptr;
+	MyMesh* m_pMesh37 = nullptr;
+
+	MyMesh* m_pMesh38 = nullptr;
+	MyMesh* m_pMesh39 = nullptr;
+
+	MyMesh* m_pMesh40 = nullptr;
+	MyMesh* m_pMesh41 = nullptr;
+
+	MyMesh* m_pMesh42 = nullptr;
+	MyMesh* m_pMesh43 = nullptr;
+
+	MyMesh* m_pMesh44 = nullptr;
+	MyMesh* m_pMesh45 = nullptr;
+
+	MyMesh* m_pMesh46 = nullptr;
+
 
 	String m_sProgrammer = "Alberto Bobadilla - labigm@rit.edu";
 private:
