48ad2d3e399af6222d877aa2cae29b549de6275f
thereseh <tah5279@g.rit.edu>
2018-02-21 11:14:31 -0500

    E06 Deliverable

--- E06 - LERP/AppClass.cpp
+++ E06 - LERP/AppClass.cpp
@@ -58,17 +58,22 @@ void Application::Display(void)
 
 	//calculate the current position
 	vector3 v3CurrentPos;
+
+	// get beginning and end destination
 	vector3 v3Begin = m_stopsList[i_index];
 	vector3 v3End;
 
+	// if beginning pos is the last point, then end pos has to be the first point
 	if (i_index == m_stopsList.size() - 1) {
 		v3End = m_stopsList[0];
 	} else {
 		v3End = m_stopsList[i_index + 1];
 	}
 
+	// get the percentage of time that has been traveled
 	f_percentage = MapValue(fTimer, 0.0f, f_totalTimeSec, 0.0f, 1.0f);
 
+	// lerp using the beginning position, end position and how much percentage that is left for it to travel between these points
 	v3CurrentPos = glm::lerp(v3Begin, v3End, f_percentage);
 	matrix4 m4Model = glm::translate(v3CurrentPos);
 
@@ -76,8 +81,11 @@ void Application::Display(void)
 	if (f_percentage >= 1.0f) {
 		i_index++;
 
+		// reset timer
 		fTimer = m_pSystem->GetDeltaTime(uClock);
 
+		// if index is out of bounds (since size is +1)
+		// then set to 0
 		if (i_index == m_stopsList.size()) {
 			i_index = 0;
 		}
