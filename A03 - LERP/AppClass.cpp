bae315d6ca43cfa48a6492a6681b17497b18bc80
thereseh <tah5279@g.rit.edu>
2018-02-25 21:32:32 -0500

    A03 Deliverable

--- A03 - LERP/AppClass.cpp
+++ A03 - LERP/AppClass.cpp
@@ -2,7 +2,7 @@
 void Application::InitVariables(void)
 {
 	//Change this to your name and email
-	m_sProgrammer = "Alberto Bobadilla - labigm@rit.edu";
+	m_sProgrammer = "Therese Henriksson - tah5279@rit.edu";
 	
 	//Set the position and target of the camera
 	//(I'm at [0,0,10], looking at [0,0,0] and up is the positive Y axis)
@@ -36,9 +36,30 @@ void Application::InitVariables(void)
 	{
 		vector3 v3Color = WaveLengthToRGB(uColor); //calculate color based on wavelength
 		m_shapeList.push_back(m_pMeshMngr->GenerateTorus(fSize, fSize - 0.1f, 3, i, v3Color)); //generate a custom torus and add it to the meshmanager
+
+		// holds stops for each orbit
+		std::vector<vector3> v3stops;
+
+		float f_center_tube = (fSize - (fSize - 0.1f)) * 0.5f;
+		// get the radius
+		float f_radius = fSize - f_center_tube;
+		
+		//Get the size of each segment
+		float f_stepSize = 2 * PI / i;
+
+		for (int j = 0; j < i; j++)
+		{
+			//Add each position gotten from the low res circle
+			v3stops.push_back(vector3(f_radius * cos(f_stepSize * j), f_radius * sin(f_stepSize * j), 0.0f));
+		}
+
+		// Add the vector containing all the stops for this current orbit
+		m_stopsList.push_back(v3stops);
+
 		fSize += 0.5f; //increment the size for the next orbit
 		uColor -= static_cast<uint>(decrements); //decrease the wavelength
 	}
+
 }
 void Application::Update(void)
 {
@@ -62,19 +83,50 @@ void Application::Display(void)
 	/*
 		The following offset will orient the orbits as in the demo, start without it to make your life easier.
 	*/
-	//m4Offset = glm::rotate(IDENTITY_M4, 90.0f, AXIS_Z);
+	m4Offset = glm::rotate(IDENTITY_M4, 90.0f, AXIS_Z);
+
+	//Get a timer
+	static float fTimer = 0;						//store the new timer
+	static uint uClock = m_pSystem->GenClock();		//generate a new clock for that timer
+	fTimer += m_pSystem->GetDeltaTime(uClock);		//get the delta time for that timer
+	float f_totalTimeSec = 0.5;
+	static int i_index = 0;
 
 	// draw a shapes
 	for (uint i = 0; i < m_uOrbits; ++i)
 	{
-		m_pMeshMngr->AddMeshToRenderList(m_shapeList[i], glm::rotate(m4Offset, 90.0f, AXIS_X));
+			m_pMeshMngr->AddMeshToRenderList(m_shapeList[i], glm::rotate(m4Offset, 90.0f, AXIS_X));
 
-		//calculate the current position
-		vector3 v3CurrentPos = ZERO_V3;
-		matrix4 m4Model = glm::translate(m4Offset, v3CurrentPos);
+			//your code goes here
+			static float f_percentage = 0;
 
-		//draw spheres
-		m_pMeshMngr->AddSphereToRenderList(m4Model * glm::scale(vector3(0.1)), C_WHITE);
+			//calculate the current position
+			vector3 v3CurrentPos;
+
+			// get beginning and end destination
+			vector3 v3Begin = m_stopsList[i][i_index % m_stopsList[i].size()];
+			vector3 v3End = m_stopsList[i][(i_index + 1) % m_stopsList[i].size()];
+
+			// get the percentage of time that has been traveled
+			f_percentage = MapValue(fTimer, 0.0f, f_totalTimeSec, 0.0f, 1.0f);
+
+			// lerp using the beginning position, end position and how much percentage that is left for it to travel between these points
+			v3CurrentPos = glm::lerp(v3Begin, v3End, f_percentage);
+
+			// translate using both rotation and position
+			matrix4 m4Model = glm::translate(m4Offset, v3CurrentPos);
+
+
+			if (f_percentage >= 1.0f) {
+				i_index++;
+
+				// reset timer
+				fTimer = m_pSystem->GetDeltaTime(uClock);
+			}
+
+			//draw spheres
+			m_pMeshMngr->AddSphereToRenderList(m4Model * glm::scale(vector3(0.1)), C_WHITE);
+		
 	}
 
 	//render list call
--- A03 - LERP/AppClass.h
+++ A03 - LERP/AppClass.h
@@ -20,6 +20,7 @@ class Application
 {
 	uint m_uOrbits = 0; //number of shapes starting at 3 and increasing in sides
 	std::vector<uint> m_shapeList; //shape index for circles
+	std::vector<std::vector<vector3>> m_stopsList; // list of orbits
 	String m_sProgrammer = "Alberto Bobadilla - labigm@rit.edu";
 	
 private:
