ae598e42082203e8f31e610fe99089f3d5e7341b
thereseh <tah5279@g.rit.edu>
2018-02-26 21:04:17 -0500

    E08 Deliverable

---
+++ E08 - Gimbal Removal/AppClass.cpp
@@ -0,0 +1,56 @@
+#include "AppClass.h"
+void Application::InitVariables(void)
+{
+	//init the mesh
+	m_pMesh = new MyMesh();
+	//m_pMesh->GenerateCube(1.0f, C_WHITE);
+	m_pMesh->GenerateCone(2.0f, 5.0f, 3, C_WHITE);
+}
+void Application::Update(void)
+{
+	//Update the system so it knows how much time has passed since the last call
+	m_pSystem->Update();
+
+	//Is the arcball active?
+	ArcBall();
+
+	//Is the first person camera active?
+	CameraRotation();
+}
+void Application::Display(void)
+{
+	// Clear the screen
+	ClearScreen();
+
+	matrix4 m4View = m_pCameraMngr->GetViewMatrix();
+	matrix4 m4Projection = m_pCameraMngr->GetProjectionMatrix();
+
+	// Take axis of rotations and multiply that into a quaternion, it seem to work
+	m_qOrientation = m_qOrientation * glm::angleAxis(v3_rotationAxis.x, AXIS_X);
+	m_qOrientation = m_qOrientation * glm::angleAxis(v3_rotationAxis.y, AXIS_Y);
+	m_qOrientation = m_qOrientation * glm::angleAxis(v3_rotationAxis.z, AXIS_Z);
+	
+	m_pMesh->Render(m4Projection, m4View, ToMatrix4(m_qOrientation));
+	
+	// draw a skybox
+	m_pMeshMngr->AddSkyboxToRenderList();
+	
+	//render list call
+	m_uRenderCallCount = m_pMeshMngr->Render();
+
+	//clear the render list
+	m_pMeshMngr->ClearRenderList();
+	
+	//draw gui
+	DrawGUI();
+	
+	//end the current frame (internally swaps the front and back buffers)
+	m_pWindow->display();
+}
+void Application::Release(void)
+{
+	SafeDelete(m_pMesh);
+
+	//release GUI
+	ShutdownGUI();
+}
